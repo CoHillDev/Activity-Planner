df1d10c2eddfbb1cfbd1f7b59e3d6504bbd774df
CoHillDev <[colinemv.dev@gmail.com](mailto:colinemv.dev@gmail.com)>
2023-11-10 01:33:05 +0100

    Registration Support Contact

--- api/src/Controller/RegistrationController.php
+++ api/src/Controller/RegistrationController.php
@@ -48,7 +48,7 @@ class RegistrationController extends AbstractController
             // generate a signed url and email it to the user
             $this->emailVerifier->sendEmailConfirmation('app_verify_email', $user,
                 (new TemplatedEmail())
-                    ->from(new Address('support@aictivityplanner.io', 'AIctivity Planner'))
+                    ->from(new Address('support@activityplanner.io', 'Activity Planner'))
                     ->to($user->getEmail())
                     ->subject('Please Confirm your Email')
                     ->htmlTemplate('registration/confirmation_email.html.twig')
